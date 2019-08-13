f47c852ef6e83d375c44a88c1d2cc2a2d4478200
Matt Chapman <chapmancac@gmail.com>
2019-08-13 13:25:58 -0400

    IDK more shit

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Matt\PycharmProjects\website\venv"
+$env:VIRTUAL_ENV="C:\Users\chapmanm3\website\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
